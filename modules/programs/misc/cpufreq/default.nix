954d3fc2a7afc544b611bd9ad80dae56d6f5653b
Sijan Lamichhane Magar <thapasijan329@gmail.com>
2025-03-18 23:31:22 +0545

    ref(cpu): reduce cpu performance on balanced mood

--- modules/programs/misc/cpufreq/default.nix
+++ modules/programs/misc/cpufreq/default.nix
@@ -3,14 +3,33 @@
     enable = true;
     settings = {
       charger = {
-        governor = "performance";
+        governor = "ondemand";  # Balanced mode for charger
         turbo = "auto";
       };
       battery = {
-        governor = "schedutil";
+        governor = "ondemand";  # Balanced mode for battery
         scaling_max_freq = 3800000;
         turbo = "never";
       };
     };
   };
 }
+
+
+# for hyper power
+# {...}: {
+#   services.auto-cpufreq = {
+#     enable = true;
+#     settings = {
+#       charger = {
+#         governor = "performance";
+#         turbo = "auto";
+#       };
+#       battery = {
+#         governor = "schedutil";
+#         scaling_max_freq = 3800000;
+#         turbo = "never";
+#       };
+#     };
+#   };
+# }
