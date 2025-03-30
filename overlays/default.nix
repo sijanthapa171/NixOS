eb8fef410265bd540f2c0157aa164efe842ed992
Sijan Lamichhane Magar <thapasijan329@gmail.com>
2025-03-30 17:57:53 +0545

    feat(rofi): add rofi plugins

--- overlays/default.nix
+++ overlays/default.nix
@@ -1,5 +1,17 @@
-{inputs, ...}: {
+{
+  inputs,
+  settings,
+  ...
+}: {
+  # Overlay custom derivations into nixpkgs so you can use pkgs.<pname>
   # additions = final: _prev: import ../pkgs {pkgs = final;};
+  additions = final: prev: {
+    pokego = prev.callPackage ../pkgs/pokego.nix {};
+    lact = prev.callPackage ../pkgs/lact.nix {};
+    sddm-astronaut = prev.callPackage ../pkgs/sddm-themes/astronaut.nix {
+      theme = settings.sddmTheme;
+    };
+  };
 
   # https://wiki.nixos.org/wiki/Overlays
   modifications = final: prev: {
@@ -9,9 +21,5 @@
       config.allowUnfree = true;
       config.nvidia.acceptLicense = true;
     };
-
-    # example = prev.example.overrideAttrs (oldAttrs: let ... in {
-    # ...
-    # });
   };
 }
