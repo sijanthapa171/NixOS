253cd884616eb3e3dbe62a4eb6ac423340b41a81
sijanthapa171 <thapasijan329@gmail.com>
2025-06-18 13:47:22 +0545

    feat: fix my timezone

--- flake.nix
+++ flake.nix
@@ -59,7 +59,7 @@
       videoDriver = "nvidia"; # CHOOSE YOUR GPU DRIVERS (nvidia, amdgpu or intel)
       hostname = "NixOS"; # CHOOSE A HOSTNAME HERE
       locale = "en_US.UTF-8"; # CHOOSE YOUR LOCALE
-      timezone = "Asia/kathmandu"; # CHOOSE YOUR TIMEZONE
+      timezone = "Asia/Kathmandu"; # CHOOSE YOUR TIMEZONE
       kbdLayout = "us"; # CHOOSE YOUR KEYBOARD LAYOUT
       kbdVariant = ""; # CHOOSE YOUR KEYBOARD VARIANT (Can leave empty)
       consoleKeymap = "us"; # CHOOSE YOUR CONSOLE KEYMAP (Affects the tty?)
