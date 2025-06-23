1c2ef7ac3c047f43778c3404f056ce74ccea7132
sijanthapa171 <thapasijan329@gmail.com>
2025-06-23 17:40:30 +0545

    feat: add local nixvim

--- flake.nix
+++ flake.nix
@@ -12,10 +12,10 @@
       url = "github:nix-community/nix-index-database";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    nixvim = {
-      url = "github:Sly-Harvey/nixvim";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    # nixvim = {
+    #   url = "github:Sly-Harvey/nixvim";
+    #   inputs.nixpkgs.follows = "nixpkgs";
+    # };
     spicetify-nix = {
       url = "github:Gerg-L/spicetify-nix";
       inputs.nixpkgs.follows = "nixpkgs";
--- modules/programs/editor/nixvim/default.nix
+++ modules/programs/editor/nixvim/default.nix
@@ -1,12 +1,55 @@
 {
   inputs,
   pkgs,
+  system,
   ...
 }: {
+  imports = [
+    inputs.nixvim.homeManagerModules.nixvim
+  ];
+
+  programs.nixvim = {
+    enable = true;
+    
+    # Import the configuration from the cloned repository
+    imports = [
+      ../nixvim-repo/config
+    ];
+
+    # You can add your own customizations here
+    colorschemes.tokyonight = {
+      enable = true;
+      style = "storm";
+      transparent = true;
+    };
+
+    globals.mapleader = " "; # Set leader key to space
+
+    options = {
+      number = true;
+      relativenumber = true;
+      shiftwidth = 2;
+      tabstop = 2;
+      expandtab = true;
+      smartindent = true;
+      wrap = false;
+      undofile = true;
+      termguicolors = true;
+      scrolloff = 8;
+    };
+  };
+
+  # Also include the package in home.packages
   home-manager.sharedModules = [
     (_: {
       home.packages = with pkgs; [
         inputs.nixvim.packages.${system}.default
+        # Additional dependencies for better experience
+        ripgrep
+        fd
+        tree-sitter
+        nodejs # Required for some LSP features
+        git # Required for some plugins
       ];
     })
   ];
