7585764b5b172b6f0bee2c44fe9c5452e3e5192e
sijanthapa171 <thapasijan329@gmail.com>
2025-10-06 10:37:29 +0545

    feat: add nixos config on main pc

---
+++ hosts/auth/variables.nix
@@ -0,0 +1,25 @@
+{
+  # That is a main pc Configuration
+  username = "sijanthapa"; # Your username (auto-set with install.sh, live-install.sh, rebuild)
+  desktop = "hyprland"; # Options: hyprland, i3-gaps, gnome, plasma6
+  terminal = "kitty"; # Options: kitty, alacritty
+  editor = "nixvim"; # Options: nixvim, vscode, helix, nvchad, neovim, emacs (WIP)
+  browser = "zen"; # Options: firefox, floorp, zen
+  tuiFileManager = "yazi"; # Options: yazi, lf
+  sddmTheme = "astronaut"; # Options: astronaut, black_hole, purple_leaves, jake_the_dog, hyprland_kath
+  wallpaper = "kurzgesagt"; # See modules/themes/wallpapers for options
+  shell = "zsh"; # Options: zsh, bash
+  games = true; # Whether to enable the gaming module
+
+  # Hardware Configuration
+  videoDriver = "nvidia"; # CRITICAL: Choose your GPU driver (nvidia, amdgpu, intel)
+  hostname = "auth"; # Your system hostname
+
+  # Localization
+  clock24h = true; # 24H or 12H clock in waybar
+  locale = "en_US.UTF-8"; # System locale
+  timezone = "Asia/Kathmandu"; # Your timezone
+  kbdLayout = "us"; # Keyboard layout
+  kbdVariant = ""; # Keyboard variant (can be empty)
+  consoleKeymap = "us"; # TTY keymap
+}
