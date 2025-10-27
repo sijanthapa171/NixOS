a7521b31b587c046e5ed628cdd0ac3f95fd6c624
sijanthapa171 <thapasijan329@gmail.com>
2025-10-27 21:14:01 +0545

    ref: removed window buttons from zen

--- modules/programs/browser/zen/settings.nix
+++ modules/programs/browser/zen/settings.nix
@@ -12,6 +12,9 @@ in
   "zen.view.use-single-toolbar" = false;
   "zen.view.sidebar-expanded" = false;
 
+  "zen.view.compact.hide-toolbar" = true;
+  "zen.view.compact.hide-tabbar" = true;
+
   "zen.watermark.enabled" = false;
   "zen.welcome-screen.seen" = true;
 
@@ -25,13 +28,6 @@ in
   "privacy.userContext.enabled" = true;
   "privacy.userContext.ui.enabled" = true;
   "privacy.userContext.longPressBehavior" = 2;
-
-  # Performance settings
-  # "gfx.webrender.all" = true; # Force enable GPU acceleration
-  # "media.ffmpeg.vaapi.enabled" = true;
-  # "widget.dmabuf.force-enabled" = true; # Required in recent Firefoxes
-  # "reader.parse-on-load.force-enabled" = true;
-  # "privacy.webrtc.legacyGlobalIndicator" = false;
 
   # Smooth Scroll
   "general.smoothScroll" = true;
@@ -147,6 +143,7 @@ in
   # General settings
   "ui.key.accelKey" = 17; # Set CTRL as master key
   "intl.locale.requested" = "en-GB,en-US";
+  "browser.tabs.inTitlebar" = 0;
   "browser.aboutConfig.showWarning" = lock-false;
   "browser.aboutwelcome.enabled" = lock-false;
   "browser.tabs.firefox-view" = lock-false;
