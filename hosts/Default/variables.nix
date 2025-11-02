9bc13c65be1f12c04375ae3b69f89cea04612df0
sijanthapa171 <thapasijan329@gmail.com>
2025-11-02 20:54:53 +0545

    ref: refactored variables.nix and added waybarTheme option

--- hosts/Default/variables.nix
+++ hosts/Default/variables.nix
@@ -1,26 +1,32 @@
 {
-  # User Configuration
-  username = "sijanthapa"; # Your username (auto-set with install.sh, live-install.sh, rebuild)
-  desktop = "hyprland"; # Options: hyprland, i3-gaps, gnome, plasma6
-  terminal = "kitty"; # Options: kitty, alacritty
-  editor = "nixvim"; # Options: nixvim, vscode, helix, doom-emacs, nvchad, neovim
-  browser = "firefox"; # Options: firefox, floorp, zen
-  tuiFileManager = "yazi"; # Options: yazi, lf
-  sddmTheme = "astronaut"; # Options: astronaut, black_hole, purple_leaves, jake_the_dog, hyprland_kath
-  defaultWallpaper = "kurzgesagt.webp"; # to change wallpaper: SUPER + SHIFT + W
-  hyprlockWallpaper = "evening-sky.webp"; # See modules/themes/wallpapers for options
-  shell = "zsh"; # Options: zsh, bash
-  games = true; # Whether to enable the gaming module
+  username = "sijanthapa"; # auto-set with install.sh, live-install.sh, and rebuild scripts.
 
-  # Hardware Configuration
-  videoDriver = "intel"; # CRITICAL: Choose your GPU driver (nvidia, amdgpu, intel)
-  hostname = "NixOS"; # Your system hostname
+  # Desktop Environment
+  desktop = "hyprland"; # hyprland, i3-gaps, gnome, plasma6
+
+  # Theme & Appearance
+  waybarTheme = "stylish"; # stylish, minimal
+  sddmTheme = "astronaut"; # astronaut, black_hole, purple_leaves, jake_the_dog, hyprland_kath
+  defaultWallpaper = "kurzgesagt.webp"; # Change with SUPER + SHIFT + W
+  hyprlockWallpaper = "evening-sky.webp";
+
+  # Default Applications
+  terminal = "kitty"; # kitty, alacritty
+  editor = "nixvim"; # nixvim, vscode, helix, doom-emacs, nvchad, neovim
+  browser = "zen"; # zen, firefox, floorp
+  tuiFileManager = "yazi"; # yazi, lf
+  shell = "zsh"; # zsh, bash
+
+  # Hardware
+  hostname = "NixOS";
+  videoDriver = "intel"; # nvidia, amdgpu, intel
+  games = true;
 
   # Localization
-  clock24h = true; # 24H or 12H clock in waybar
-  locale = "en_US.UTF-8"; # System locale
-  timezone = "Asia/Kathmandu"; # Your timezone
-  kbdLayout = "us"; # Keyboard layout
-  kbdVariant = ""; # Keyboard variant (can be empty)
-  consoleKeymap = "us"; # TTY keymap
+  timezone = "Asia/Kathmandu";
+  locale = "en_US.UTF-8";
+  clock24h = true;
+  kbdLayout = "us";
+  kbdVariant = "";
+  consoleKeymap = "us";
 }
