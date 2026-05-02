8cc171204a7aa75603e286bf7cdb211aa6dfcada
sijanthapa171 <thapasijan329@gmail.com>
2026-05-02 21:01:29 +0545

    [FEAT]: enable obs

--- hosts/Default/configuration.nix
+++ hosts/Default/configuration.nix
@@ -48,7 +48,7 @@ in
     ../../modules/programs/media/spicetify
     # ../../modules/programs/media/youtube-music
     # ../../modules/programs/media/thunderbird
-    # ../../modules/programs/media/obs-studio
+    ../../modules/programs/media/obs-studio
     ../../modules/programs/media/mpv
     ../../modules/programs/misc/tlp
     ../../modules/programs/misc/lact # GPU fan, clock and power configuration
