322a87e96a8d36cd21579c7ab525adbbfb1343d1
sijanthapa171 <thapasijan329@gmail.com>
2026-06-10 20:17:16 +0545

    [FEAT]: add bat pkg

--- modules/core/packages.nix
+++ modules/core/packages.nix
@@ -68,7 +68,7 @@
     # waypaper # Change wallpaper
     # wget # Tool For Fetching Files With Links
     # ytmdl # Tool For Downloading Audio From YouTube
-
+    bat 
     # devenv
     # devbox
     # shellify
