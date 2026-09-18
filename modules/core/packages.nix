be1d395fcc7594b8279ad284166fda6780ec7d16
sijanthapa171 <thapasijan329@gmail.com>
2026-09-18 20:54:13 +0545

    chore(core): add scrcpy to system packages

--- modules/core/packages.nix
+++ modules/core/packages.nix
@@ -13,6 +13,7 @@
   nixpkgs.config.allowUnfree = true;
 
   environment.systemPackages = with pkgs; [
+    scrcpy # Display and control Android devices over USB or TCP/IP
     appimage-run # Needed For AppImage Support
     killall # For Killing All Instances Of Programs
     lm_sensors # Used For Getting Hardware Temps
